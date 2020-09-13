0b5425998cf5151a6e9bfaba36d09fe908827c6a
agibbs22 <agibbs22@amherst.edu>
2020-09-13 01:05:10 +0000

    Initial edits.

--- string-length.asm
+++ string-length.asm
@@ -12,5 +12,18 @@
 
 ;;; COMPLETE THE string_length FUNCTION.
 string_length:
+	;; have pointer to beginning of string
+	;; loop through each char in the string
+	;; if char equals zero-valued byte (/0) jump (je) to end
+	;; have counter that gets incremented for each loop iteration
+	;; return counter
+	mov 	ecx, 0			; create counter
+top:
+	cmp 	string, "0"		; see if char equals zero-valued byte
+	je 	end			; jump to end if char equals zero-valued byte
+	inc	ecx			; increment counter if we haven't reached end of string
+	jmp 	top			; jump to top to check next char
+end:
+	ret				; return counter
 
 ;;; ----------------------------------------------------------------------------------------
